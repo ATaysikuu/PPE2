2b18a94ccba9fcd13ce59682c08cf78411cffcb5
Unknown <ataysikuu@gmail.com>
2019-04-17 22:45:10 +0200

    Updated for functions

--- php/validationformation.php
+++ php/validationformation.php
@@ -5,13 +5,15 @@ echo($_SESSION['pseudo']);
 if(CheckAdmin($_SESSION['pseudo'])){
     if($_GET['action']=='ok'){
         $idformation=$_GET['id'];
-        $reqAcceptFormation=$bdd->query('UPDATE articles SET validation = "1" where id_article = "'.$idformation.'"');
+        ValidateCourse($idformation);
+        //$reqAcceptFormation=$bdd->query('UPDATE articles SET validation = "1" where id_article = "'.$idformation.'"');
         //header("Location: /admin/formationlist.php");
         echo('<script>document.location="/admin/formationlist.php"</script>');
     }
     if($_GET['action']=='no'){
         $idformation=$_GET['id'];
-        $reqAcceptFormation=$bdd->query('DELETE FROM articles where id_article = "'.$idformation.'"');
+        DeleteCourse($idformation);
+        //$reqAcceptFormation=$bdd->query('DELETE FROM articles where id_article = "'.$idformation.'"');
         //header("Location: /admin/formationlist.php");
         echo('<script>document.location="/admin/formationlist.php"</script>');
     }
