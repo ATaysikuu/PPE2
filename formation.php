939ac6c7abd222daf84495b59fa7c607fbe2e0e2
Unknown <ataysikuu@gmail.com>
2019-04-24 10:29:30 +0200

    update

--- formation.php
+++ formation.php
@@ -1,31 +1,37 @@
-<?php session_start();?>
-<!DOCTYPE html>
-<head>
-</head>
-<?php require_once($_SERVER['DOCUMENT_ROOT'].'/include/header.php'); ?>
-<?php
-
-    try {
-        require_once($_SERVER['DOCUMENT_ROOT']."/php/config.php");
-        $req=$bdd->prepare('SELECT * FROM articles WHERE id_article = :id_art_Req');
-        $idArticle = $_GET['id'];
-        
-        $req->execute(array('id_art_Req' => $idArticle));
-        while($result=$req->fetch()){
-            $articleName = $result['name_article'];
-            $articleDescription = $result['description_article'];
-            $articlePrice = $result['price_article'];
-        }
-    }
-    catch (Exception $e){
-        echo($e);
-    }
+<?php session_start() ?>
+<?php 
+	require_once($_SERVER['DOCUMENT_ROOT'].'/php/functions.php');
+	$courseData=GetCourse($_GET['id']);
 ?>
-
-<div class="product_info">
-    <h3><?php echo($articleName);?></h3><br/>
-    <h4><?php echo($articleDescription);?></h4><br/>
-    <h4><?php echo($articlePrice);?></h4><br/>
-</div>
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php echo($courseData['name_article'])?></title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="/css/style.css">
+	<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+</head>
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/include/header.php') ?>
+<body>
+	<div class="container">
+		<div class="wrapper">
+			<div id="formation_title" class="row">
+				<h2><?php echo($courseData['name_article'])?></h2>
+				<h5>Auteur: <?php echo($courseData['pseudo_member']);?>
+			</div>
+			<div id="formation_content" class="row">
+				<!-- php TCHOU POUR METTRE LE CONTENU DE LA FORMATION  -->
+				<h4>Contenu de la formation</h4><br/>
+				<p><?php echo($courseData['description_article'])?></p>
+			</div>
+			<div id="formation_date" class="row">
+				<p>Date de mise en ligne: <?php echo($courseData['date_article'])?></p>
+			</div>
+			<div id="formation_price" class="row">
+				<p>Prix: <?php echo($courseData['price_article'])?></p>
+		</div>
+            </div>
+	</div>
+</body>
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/include/footer.php') ?>
 </html>
-<?php require_once($_SERVER['DOCUMENT_ROOT'].'/include/footer.php'); ?>
