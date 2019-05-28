ae533860d1b4ebedccbc4f0b84ba49d8e385fbf8
Seline00 <38215655+Seline00@users.noreply.github.com>
2019-05-28 14:24:53 +0200

    Update contact.php
    Création page contact.php avec formulaire

--- contact.php
+++ contact.php
@@ -4,13 +4,46 @@
 <html>
 <head>
 	<title>Contact</title>
-	<link rel="stylesheet" type="text/css" href="../css/style.css">
-	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="/css/style.css">
+	<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
 	<meta charset="utf-8">
 </head>
 <?php require_once($_SERVER['DOCUMENT_ROOT'].'/include/header.php') ?> <!-- rajout du header -->
 <body>
-	
+	<?php
+		if(isset($_POST['submit']))
+		{
+			if(!empty($_POST['pseudo']) AND !empty($_POST['email']) AND !empty($_POST['commentaire']))
+				{
+
+				}
+			else
+				{
+					$msg="Remplissez tous les champs svp";
+				}
+		}
+		
+	?>
+
+	<div class="container">
+
+		<div id="formulaire_contact">
+			<h4>Formulaire de contact</h4>
+				<form id="formulaire" action="" method=POST>
+					<input type="text" id="fid" name="pseudo" placeholder="pseudo">
+					<input type="email" id="fmail" name="email" placeholder="email"><br><br>
+					<textarea type="text" id="fcomment" name="commentaire" placeholder="commentaire"></textarea> <br><br>
+					<input type="submit" id="fsubmit" name="submit" placeholder="Envoyer">
+				</form>
+				<?php
+					if(isset($msg))
+					{
+						echo $msg;
+					}
+				?>
+		</div>
+
+	<div>
 
 </body>
 	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/include/footer.php') ?> <!-- rajout du footer -->
